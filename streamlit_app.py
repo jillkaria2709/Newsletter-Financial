bedf6ede684955dd4f0da8fd370d20b955544458
jillkaria2709 <157040744+jillkaria2709@users.noreply.github.com>
2024-12-01 18:31:35 +0000

    Final

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ alpha_vantage_key = st.secrets["alpha_vantage"]["api_key"]
 openai.api_key = st.secrets["openai"]["api_key"]
 
 # Streamlit App Title
-st.title("Alpha Vantage Multi-Agent System with RAG, Bespoke Labs, Chatbot, and More")
+st.title("Financial Nrwsletter using Multi-Agent Newsletter")
 
 ### Define Function Schema ###
 tools = [
